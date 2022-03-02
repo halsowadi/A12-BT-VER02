9f2fc882391e06807ac3b1153f3c42a96f3ca6ce
halsowadi <halsowadi1@gmail.com>
2022-03-02 13:41:09 -0500

    Initial commit

---
+++ app/src/main/java/com/example/a12_bt_ver02/MainActivity.java
@@ -0,0 +1,189 @@
+package com.example.a12_bt_ver02;
+/*
+Hussein Alsowadi
+Last Updated: 1/23/22
+Template App
+ */
+
+import com.example.a12_bt_ver02.databinding.ActivityMainBinding;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.UUID;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+
+public class MainActivity extends AppCompatActivity {
+    // BT Variables
+    private final String CV_ROBOTNAME = "EV3A";
+    private BluetoothAdapter cv_btInterface = null;
+    private Set<BluetoothDevice> cv_pairedDevices = null;
+    private BluetoothDevice cv_btDevice = null;
+    private BluetoothSocket cv_btSocket = null;
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        ////setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+        cpf_checkBTPermissions();
+
+        //for button
+        binding.button.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        //Implement event handling
+                        //// Context ctx =  MainActivity.this;
+                        //// Toast.makeText(ctx, "Hello Hussein Alsowadi", Toast.LENGTH_LONG).show();
+                       // binding.textView.setText(R.string.Hello_426);
+                    }
+                });
+
+        //setting fab press
+        binding.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Different way to show fab message
+                //// Snackbar.make(view, R.string.fab, Snackbar.LENGTH_SHORT).setAction("Action", null).show();
+
+                Toast.makeText(getApplicationContext(), R.string.fab, Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+    }
+
+
+
+    // Overriding onCreateoptionMenu() to make Option menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //Inflating menu by overriding inflate() method of MenuInflater class.
+        //Inflating here means parsing layout XML to views.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+    //Overriding onOptionsItemSelected to perform event on menu items
+    //Setting Menu text when a option is selected
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
+        ////Toast.makeText(this, "You chose : " + menuItem.getTitle(), Toast.LENGTH_SHORT).show();
+        switch (menuItem.getItemId()) {
+            case R.id.menu_first:
+
+                //Your code here
+                cpf_requestBTPermissions();
+                return true;
+            case R.id.menu_second:
+                //Your code here
+                cv_btDevice = cpf_locateInPairedBTList(CV_ROBOTNAME);
+                return true;
+            case R.id.menu_third:
+                //Your code here
+                cpf_connectToEV3(cv_btDevice);
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(menuItem);
+        }
+    }
+    private void cpf_checkBTPermissions() {
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.BLUETOOTH_SCAN) == PackageManager.PERMISSION_GRANTED) {
+            binding.vvTvOut1.setText("BLUETOOTH_SCAN already granted.\n");
+        }
+        else {
+            binding.vvTvOut1.setText("BLUETOOTH_SCAN NOT granted.\n");
+        }
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.BLUETOOTH_CONNECT) == PackageManager.PERMISSION_DENIED) {
+            binding.vvTvOut2.setText("BLUETOOTH_CONNECT NOT granted.\n");
+        }
+        else {
+            binding.vvTvOut2.setText("BLUETOOTH_CONNECT already granted.\n");
+        }
+    }
+
+    // https://www.geeksforgeeks.org/android-how-to-request-permissions-in-android-application/
+    private void cpf_requestBTPermissions() {
+        // We can give any value but unique for each permission.
+        final int BLUETOOTH_SCAN_CODE = 100;
+        final int BLUETOOTH_CONNECT_CODE = 101;
+
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.BLUETOOTH_SCAN) == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.BLUETOOTH_SCAN},
+                    BLUETOOTH_SCAN_CODE);
+        }
+        else {
+            Toast.makeText(MainActivity.this,
+                    "BLUETOOTH_SCAN already granted", Toast.LENGTH_SHORT) .show();
+        }
+
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.BLUETOOTH_CONNECT) == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.BLUETOOTH_CONNECT},
+                    BLUETOOTH_CONNECT_CODE);
+        }
+        else {
+            Toast.makeText(MainActivity.this,
+                    "BLUETOOTH_CONNECT already granted", Toast.LENGTH_SHORT) .show();
+        }
+    }
+    // Modify from chap14, pp390 findRobot()
+    private BluetoothDevice cpf_locateInPairedBTList(String name) {
+        BluetoothDevice lv_bd = null;
+        try {
+            cv_btInterface = BluetoothAdapter.getDefaultAdapter();
+            cv_pairedDevices = cv_btInterface.getBondedDevices();
+            Iterator<BluetoothDevice> lv_it = cv_pairedDevices.iterator();
+            while (lv_it.hasNext())  {
+                lv_bd = lv_it.next();
+                if (lv_bd.getName().equalsIgnoreCase(name)) {
+                    binding.vvTvOut1.setText(name + " is in paired list");
+                    return lv_bd;
+                }
+            }
+            binding.vvTvOut1.setText(name + " is NOT in paired list");
+        }
+        catch (Exception e) {
+            binding.vvTvOut1.setText("Failed in findRobot() " + e.getMessage());
+        }
+        return null;
+    }
+
+    // Modify frmo chap14, pp391 connectToRobot()
+    private void cpf_connectToEV3(BluetoothDevice bd) {
+        try  {
+            cv_btSocket = bd.createRfcommSocketToServiceRecord
+                    (UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"));
+            cv_btSocket.connect();
+            binding.vvTvOut2.setText("Connect to " + bd.getName() + " at " + bd.getAddress());
+        }
+        catch (Exception e) {
+            binding.vvTvOut2.setText("Error interacting with remote device [" +
+                    e.getMessage() + "]");
+        }
+    }
+
+
+}
